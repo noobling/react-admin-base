8668ebe9131b2397be7c6b3d380af2eb35218da5
David Yu <david.yu@readify.net>
2019-03-11 20:39:06 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React from 'react'
-import {Admin, Resource} from 'react-admin'
+import { Admin, Resource, ListGuesser } from 'react-admin'
 import buildGraphcoolProvider from 'ra-data-graphcool'
+import users from './components/users'
+import posts from './components/posts'
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super()
-    this.state = { dataProvider: null}
+    this.state = { dataProvider: null }
   }
 
   async componentDidMount() {
-    const dataProvider = await buildGraphcoolProvider({clientOptions: {uri: }})
+    const dataProvider = await buildGraphcoolProvider({
+      clientOptions: {
+        uri: 'https://api.graph.cool/simple/v1/cjt49cvhf3nb001897ecqnyqs'
+      }
+    })
 
-    this.setState({dataProvider})
+    this.setState({ dataProvider })
   }
 
   render() {
-    const { dataProvider } = this.state;
+    const { dataProvider } = this.state
 
     if (!dataProvider) {
-        return <div>Loading</div>;
+      return <div>Loading</div>
     }
 
     return (
-        <Admin dataProvider={dataProvider}>
-            <Resource name="Post" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />
-        </Admin>
-    );
+      <Admin dataProvider={dataProvider}>
+        {/* <Resource name="Post" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} /> */}
+        <Resource name="User" list={ListGuesser} {...users} />
+        <Resource name="Post" list={ListGuesser} {...posts} />
+      </Admin>
+    )
   }
 }
